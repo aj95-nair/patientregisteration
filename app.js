3bc6bbe824520bbe5feedba60aefe3a3c364eacc
aj95-nair <aj95.nair@gmail.com>
2022-08-11 10:47:22 +0400

    1st commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const Logger = require("morgan");
+const Parser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const patientRoutes = require('./routes/patient');
+const CORS = require('cors')
+const router = express.Router();
+
+mongoose.connect(
+  "mongodb+srv://admin:admin@hospital-management.xkqgfiw.mongodb.net/?retryWrites=true&w=majority"
+  //"mongodb://35.202.7.81:27017/hospital"
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+
+app.use(Logger("dev"));
+
+//app.use('/uploads', express.static('uploads'));
+app.use(Parser.urlencoded({ extended: false }));
+app.use(Parser.json());
+app.use(CORS());
+/*app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+*/
+app.use(passport.initialize());
+require('./middlewares/passport')(passport);
+app.use("/users", patientRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
